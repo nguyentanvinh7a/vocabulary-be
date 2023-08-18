2b6fff7e24ee566874ae6702d495810845427087
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2023-08-19 00:26:05 +0700

    fix API

--- controllers/auth/verifyToken.js
+++ controllers/auth/verifyToken.js
@@ -1,8 +1,10 @@
 const CognitoJwtVerifier = require("aws-jwt-verify").CognitoJwtVerifier;
 
+const ROLE_LIST = require('../../config/roles_list');
+
 const cognitoJwtVerifier = new CognitoJwtVerifier({
     userPoolId: "ap-southeast-1_P5gSpN7to",
-    tokenUse: "id",
+    tokenUse: "access",
     clientId: "4n0aq1f9rmn0bvrum0ts33igaj",
 });
 
@@ -16,8 +18,8 @@ exports.isAuth = async (req, res, next) => {
     try {
         const decoded = await cognitoJwtVerifier.verify(token);
         req.userId = decoded.sub;
-        req.user = decoded['cognito:username'];
-        req.roles = decoded['cognito:groups']?.includes('admin') ? ['admin', 'user'] : ['user'];
+        req.user = decoded.username;
+        req.roles = decoded['cognito:groups']?.includes(ROLE_LIST.ADMIN) ? [ROLE_LIST.ADMIN] : [ROLE_LIST.USER];
         next();
     } catch (err) {
         return res.status(401).json({ msg: err.message });
