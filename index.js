00ec230948009dd423d533b25f92e7eb07aa2638
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2023-10-15 09:41:09 +0700

    add cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const http = require("http");
-const cors = require('cors');
+var cors = require('cors');
 
 const db = require('./config/db');
 var mongoose = require('mongoose');
