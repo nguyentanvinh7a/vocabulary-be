c3bb75178ed810f08159e77f0b6a9f6951d634e4
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2023-10-15 17:26:34 +0700

    fix aws error at server

--- controllers/auth/verifyToken.js
+++ controllers/auth/verifyToken.js
@@ -1,12 +1,26 @@
-const CognitoJwtVerifier = require("aws-jwt-verify").CognitoJwtVerifier;
+var CognitoJwtVerifier = require("aws-jwt-verify").CognitoJwtVerifier;
+var SimpleJwksCache = require("aws-jwt-verify/jwk").SimpleJwksCache;
+var SimpleJsonFetcher = require("aws-jwt-verify/https").SimpleJsonFetcher;
 
-const ROLE_LIST = require('../../config/roles_list');
-
-const cognitoJwtVerifier = new CognitoJwtVerifier({
+var verifier = CognitoJwtVerifier.create(
+  {
     userPoolId: "ap-southeast-1_y0cDaGZFP",
     tokenUse: "access",
     clientId: "3905akl8l4aa8tafg0sp3aq457",
-});
+  },
+  {
+    jwksCache: new SimpleJwksCache({
+      fetcher: new SimpleJsonFetcher({
+        defaultRequestOptions: {
+          responseTimeout: 3000,
+        },
+      }),
+    }),
+  }
+);
+
+const ROLE_LIST = require('../../config/roles_list');
+
 
 exports.isAuth = async (req, res, next) => {
     const token = req.header('x-auth-token');
@@ -16,7 +30,7 @@ exports.isAuth = async (req, res, next) => {
     }
 
     try {
-        const decoded = await cognitoJwtVerifier.verify(token);
+        const decoded = await verifier.verify(token);
         req.userId = decoded.sub;
         req.user = decoded.username;
         req.roles = decoded['cognito:groups']?.includes(ROLE_LIST.ADMIN) ? [ROLE_LIST.ADMIN] : [ROLE_LIST.USER];
