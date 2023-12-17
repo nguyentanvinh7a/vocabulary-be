e2c8a58632525863526986440015ebefa929187e
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2023-12-17 13:39:23 +0700

    test CI/CD

--- routes/api/health.js
+++ routes/api/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Test CI/CD 1');
+    res.send('Test CI/CD 2');
 });
 
 module.exports = router;
