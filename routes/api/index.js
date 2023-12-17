25d76dfb304e76565e9cfcffec3044847c19b427
Vinh Tan Nguyen <nguyentanvinh2911@gmail.com>
2023-12-17 10:01:47 +0700

    add API check status

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const auth = require('./auth');
 const word = require('./word');
 const list = require('./list');
 const vocabulary = require('./vocabulary');
+const health = require('./health');
 
 const router = require('express').Router();
 
@@ -9,5 +10,6 @@ router.use('/auth', auth);
 router.use('/word', word);
 router.use('/list', list);
 router.use('/vocabulary', vocabulary);
+router.use('/health', health);
 
 module.exports = router;
