755ca464db8382fd79398692c323f9d85811bae8
Vinh Nguyen <ntanvinh@tma.com.vn>
2024-02-04 22:45:47 +0700

    Update health API route message

--- routes/api/health.js
+++ routes/api/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Test CI/CD');
+    res.send('API is running');
 });
 
 module.exports = router;
